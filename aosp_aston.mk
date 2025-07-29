b9377532738283c68926d22a09a14bab2190d2b4
Venkat Yadlapati <venkat.yadlapati@hotmail.com>
2025-07-29 15:23:08 +0530

    test commit
    Change-Id: I5585c77b01041f9f751d7bce07a2abeffccc5338
    Signed-off-by: Venkat Yadlapati <venkat.yadlapati@hotmail.com>

--- aosp_aston.mk
+++ aosp_aston.mk
@@ -8,7 +8,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from aston device
+# Inherit from aston benz device
 $(call inherit-product, device/oneplus/aston/device.mk)
 
 # Inherit some common PixelOS stuff.
