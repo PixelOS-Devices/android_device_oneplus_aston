bd9532271f5f59c8780f9bb610e6030442b39e40
inferno0230 <mail@inferno0230.in>
2025-10-26 16:42:37 +0530

    aston: Opt-out of Livedisplay SE and AF
    SE: Our hbm doesn't sticks, and max brightness with slider is same as hbm anyway
    
    AF: [oplus_display_panel_set_pwm_turbo:1978] Falied to set pwm turbo status, because it is unsupport
    
    Change-Id: I682a1a03d5794d072965aa39aff3029d069c1789
    Signed-off-by: inferno0230 <mail@inferno0230.in>

--- device.mk
+++ device.mk
@@ -39,7 +39,7 @@ PRODUCT_PACKAGES += \
 $(call soong_config_set,surfaceflinger,udfps_lib,//hardware/oplus:libudfps_extension.oplus)
 
 # LiveDisplay
-$(call soong_config_set,OPLUS_LINEAGE_LIVEDISPLAY_HAL,ENABLE_AF,true)
+$(call soong_config_set,OPLUS_LINEAGE_LIVEDISPLAY_HAL,ENABLE_SE,false)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
